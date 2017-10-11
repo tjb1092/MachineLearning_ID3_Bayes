666102eb738844e9f44e86fb61e1ca3fd8b918d9
unknown <baileytj@mail.uc.edu>
2017-10-11 00:21:13 -0400

    Able to pick root attribute. May not need any more if it can perfectly classify everything with one sorting.

--- HW3.m
+++ HW3.m
@@ -2,41 +2,93 @@ load fisheriris.mat
 
 y = zeros(length(species),1);
 for i = 1:length(species)
+  %We are clumping versicolor and virginica together.
   if strcmp(species{i,1},'setosa')
     s = 1;
-  elseif strcmp(species{i,1},'versicolor')
-    s = 2;
-  else
-    s = 3;
-    
+  else 
+    s = 0;   
   end
   y(i) = s;
 end
 
-iris = [meas, y];
+% Randomly pick out ~50% of the data for training.
+randVar = rand(length(iris),1);
+Index = randVar >= 0.6; 
+
+%% L2 Regularization
+
+%Train = 60% of data
+%Test = 30% of data.
+%Index the data
+iris_train = iris(Index,:);
+
+iris_test = iris(~Index,:);
+
+
 
 for bins = 5:5:20
-  [n, x] = hist(iris(:,1),bins);
   
-  % how to compute entropy 
-  % P = [p1, ..., pn] is prob distribution
-  % log2P is a vector of log2 values: log2p1, ..., log2pn
-  % p1 x log2 p1 + ... + pn log2 pn is the dot product of these 
-  % two vectors
-  % example:
-  n
-
-  % form p:
-  p = n/sum(n)
-
-  %check that it is a prob
-  sum(p)
-
-  % form logs
-  logp = log2(p)
-
-  % Compute the entropy: - dot product
-  Entropy= -logp*p'  or Entropy = -sum(logp .* p)
-
+  is_done_training = false;
+  while(~is_done_training)
+  
+    [m_train, ~] = size(iris_train);
+    [m_pos, ~] = size(iris_train(iris_train(:,5) == 1,:));
+    [m_neg, ~] = size(iris_train(iris_train(:,5) ~= 1,:));
+        
+    p_pos = m_pos/m_train;
+    p_neg = m_neg/m_train;
+    
+    Entropy_a = nansum(-p_pos .* log2(p_pos) - p_neg .* log2(p_neg));
+    
+    %Search for best attribute.
+    for i = 1:4
+      [m_train, ~] = size(iris_train);
+      [n, x] = hist(iris_train(:,i),bins);
+      thresholds = (x(1:end-1) + x(2:end)) / 2;  
+      %For each attribute, get the arrays of values associated with each bin.
+      for j = 1:(length(thresholds)+1)
+        if j == 1
+          children{j} = iris_train((iris_train(:, i) < thresholds(j)),:);
+        elseif j == (length(thresholds)+1)
+          children{j} = iris_train((iris_train(:, i) >= thresholds(j-1)),:);
+        else
+          children{j} = iris_train(((iris_train(:, i) < thresholds(j)) & (iris_train(:, i) >= thresholds(j-1))),:);
+        end
+      end
+      
+      for k = 1:length(children)
+        temp = children{k};
+        [m_child(k), ~] = size(temp);
+        [m_pos, ~] = size(temp(temp(:,5) == 1,:));
+        [m_neg, ~] = size(temp(temp(:,5) ~= 1,:));
+        
+        p_pos(k) = m_pos/m_child(k);
+        p_neg(k) = m_neg/m_child(k);
+      end
+      T(i).n = children;
+      T(i).thresholds = thresholds;
+      
+      Entropy(i) = nansum(((m_child)./m_train) .*( -p_pos .* log2(p_pos) - p_neg .* log2(p_neg)));
+      
+    end
+    
+    InformationGain = Entropy_a - Entropy;
+    %Pick Best Attribute
+    [m_IG, attribute] = max(InformationGain);  
+    
+    
+    T = T(attribute);
+    if(m_IG == Entropy_a)
+      %Perfectly Classified
+      is_done_training = true;
+    else
+      %Needs more work
+      %From testing, each set can be linearly classified by looking @ 
+      %petal length regardless of bin size.
+      disp('HELP MAKE MY LIFE HELL...');
+    end
+  end
+  
+  %Get test performance metrics
   
 end
